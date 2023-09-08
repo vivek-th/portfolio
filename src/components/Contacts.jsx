afcdc40df5d8aeada1a01351ec165434c6dda90f
vivek_thakur <vivekthakuruspc@gmail.com>
2023-09-09 00:58:28 +0530

    contact section

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import './components.css'
+import "./components.css";
+import React from "react";
+  import MapChart from "./map";
 
-
-const Contacts = () => {
+const contacts = () => {
   return (
-    <div className='contactsection'>
-     my contacts 
+    <div className="herosection">
+      <div className="belownavbar4">
+        <div className="leftcontact">
+          <form className="contactme">
+            <h1 >Contact me</h1>
+            <input className="contactname" placeholder="Name" />
+            <input className="contactemail" placeholder="Email" />
+            <textarea className="contactmessage"placeholder="Write your message" rows={10} />
+            <button className="sendbutton">Send</button>
+          </form>
+        </div>
+        <div className="rightcontact"><MapChart/></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contacts
+export default contacts;
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,24 +2,20 @@
 import "./components.css";
 import React from "react";
 
-const data=[
-  "web dev",
-  "Game dev",
-  "Dsa and algo"
-]
+const data = ["web dev", "Game dev", "Dsa and allgo"];
 
 const Works = () => {
   return (
     <div className="herosection">
-      <div className="belownavbar2">
+      <div className="belownavbar3">
         <div className="leftwork ">
-          <list>
-            {data.map((item)=>
-              <li key={item}>{item} </li>
-            )}
-          </list>
-          <div className="rightwork">{/* 3d  model */}</div>
+          <div className="worklist">
+            {data.map((item) => (
+              <li className="workli" key={item} test={item}>{item} </li>
+            ))}
+          </div>
         </div>
+        <div className="rightwork">{/* 3d  model */}</div>
       </div>
     </div>
   );
